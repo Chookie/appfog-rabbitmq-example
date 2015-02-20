b50816007a82f0a8e1a7fcbcc74bd504d18bdeaa
Alison <alison@johnston.name>
2015-02-20 23:58:17 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express'),
+    //path = require('path'),
+    //amqp = require('amqp'),
+    bodyParser = require('body-parser'),
+    stylus = require('stylus');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//noinspection JSUnusedGlobalSymbols
+app.use(stylus.middleware (
+    {
+        src: __dirname + '/public',
+        compile: function(str, path){
+            return stylus(str).set('filename', path);
+        }
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+app.connectionStatus = 'No server connection';
+app.exchangeStatus = 'No exchange established';
+app.queueStatus = 'No queue established';
+
+app.get('*', function (req, res) {
+   res.render('index',
+       {
+           title: "Welcome to RabbitMQ and Node/Express",
+           connectionStatus: app.connectionStatus,
+           exchangeStatus: app.exchangeStatus,
+           queueStatus: app.queueStatus
+       });
+});
+
+app.listen(app.get('port'), function () {
+   console.log("RabbitMQ + Node.js app running on port %s", app.get('port'))
+});
+
